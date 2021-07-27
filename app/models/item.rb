9e52e11615c13a14259722c2ece71b22af55e382
same-jima <mba10penguin@icloud.com>
2021-07-27 19:15:21 +0900

    ActiveHash追加・モデル追加・アクションcreate追加

---
+++ app/models/item.rb
@@ -0,0 +1,15 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :area
+  belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :title, :text, :price
+    validates :category_id, :condition_id, :delivery_id, :area_id, 
+    numericality: {other_than: 1 , message: "can't be blank"}
+end
