5097ee89c8b3d037982c50af3b520bc68486ce02
same-jima <mba10penguin@icloud.com>
2021-07-28 16:35:58 +0900

    JavaScriptで価格のイベント発火完成・テストコード準備

---
+++ app/javascript/item_price.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+  
+  const pullDownButton = document.getElementById("item-price")
+
+  pullDownButton.addEventListener('click', function(){
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+      const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round( priceInput.value * 0.1 );
+    const addPofitDom = document.getElementById("profit");
+        addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+    })
+  })
+})
