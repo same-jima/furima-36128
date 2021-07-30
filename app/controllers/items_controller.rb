77847f59d2f469c06e746299a3db0d51eb484b8f
same-jima <mba10penguin@icloud.com>
2021-07-31 00:55:49 +0900

    商品一覧機能：index・controllerのparams追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -13,6 +14,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+      @items = Item.includes(:user)
       render :new
     end
   end
